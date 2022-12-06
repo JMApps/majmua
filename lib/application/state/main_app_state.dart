7460e8ae498cb9570863d8765a90ee028324ff82
jmapps <jmappsmuslim@gmail.com>
2022-12-06 20:42:51 +0300

    Added main app state

---
+++ lib/application/state/main_app_state.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MainAppState extends ChangeNotifier {
+  final _fridayController =
+      PageController(initialPage: 0, viewportFraction: 0.85);
+
+  PageController get getFridayController => _fridayController;
+
+  int _fridaySunnahControllerIndex = 0;
+
+  int get getFridaySunnahControllerIndex => _fridaySunnahControllerIndex;
+
+  set fridaySunnahControllerIndex(int index) {
+    _fridaySunnahControllerIndex = index;
+    if (_fridayController.hasClients) {
+      _fridayController.animateToPage(
+        index,
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeOut,
+      );
+    }
+    notifyListeners();
+  }
+}
