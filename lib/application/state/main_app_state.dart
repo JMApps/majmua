f87b3baf456c7f6e6c8289571c6464529676efe3
jmapps <jmappsmuslim@gmail.com>
2022-12-15 17:21:11 +0300

    Removed rest day progress

--- lib/application/state/main_app_state.dart
+++ lib/application/state/main_app_state.dart
@@ -3,13 +3,9 @@ import 'package:flutter/material.dart';
 class MainAppState extends ChangeNotifier {
   final PageController _fridayController = PageController(initialPage: 0, viewportFraction: 0.85);
 
-  final DateTime _dateTime = DateTime.now();
-
   bool _isTextPercentChange = true;
 
   bool get getIsTextPercentChange => _isTextPercentChange;
-
-  int get getDayInMinutes => _dateTime.difference(DateTime(_dateTime.year, _dateTime.month, _dateTime.day)).inMinutes;
 
   PageController get getFridayController => _fridayController;
 
@@ -17,11 +13,7 @@ class MainAppState extends ChangeNotifier {
 
   int get getFridaySunnahControllerIndex => _fridaySunnahControllerIndex;
 
-  double restDayProgress() {
-    return getDayInMinutes * 24 * 60 / 20736;
-  }
-
-  set fridaySunnahControllerIndex(int index) {
+  set setFridaySunnahControllerIndex(int index) {
     _fridaySunnahControllerIndex = index;
     if (_fridayController.hasClients) {
       _fridayController.animateToPage(
