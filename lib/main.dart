9e57f6d9d2d3f3a0a55a8a8fa2df79c11dd70d86
jmapps <jmappsmuslim@gmail.com>
2022-12-15 17:20:14 +0300

    statusBarColor transparent for android versions

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:majmua/application/root_page.dart';
 import 'package:majmua/application/state/main_app_state.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
   runApp(
     MultiProvider(
       providers: [
