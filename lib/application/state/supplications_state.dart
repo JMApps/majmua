7086a8666dba1856f84b94f2e73a42dcf794a958
jmapps <jmappsmuslim@gmail.com>
2022-12-15 17:20:37 +0300

    Fixes lightImpact

--- lib/application/state/supplications_state.dart
+++ lib/application/state/supplications_state.dart
@@ -35,6 +35,9 @@ class SupplicationsState extends ChangeNotifier {
     } else {
       _supplicationCount = 1;
     }
+    if (_isVibration) {
+      HapticFeedback.lightImpact();
+    }
     notifyListeners();
   }
 
