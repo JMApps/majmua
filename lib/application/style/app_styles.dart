dafdeee17237a200692e40ee9312b48c1c86339b
jmapps <jmappsmuslim@gmail.com>
2022-12-22 19:59:11 +0300

    Added symmetric padding's and margin's for app style class

--- lib/application/style/app_styles.dart
+++ lib/application/style/app_styles.dart
@@ -8,6 +8,14 @@ class AppStyles {
   static const mainPaddingMini = EdgeInsets.all(8);
   static const mainMargin = EdgeInsets.all(16);
   static const mainMarginMini = EdgeInsets.all(8);
+  static const symmetricHorizontalPadding = EdgeInsets.symmetric(horizontal: 16);
+  static const symmetricVerticalPadding = EdgeInsets.symmetric(vertical: 16);
+  static const symmetricHorizontalPaddingMini = EdgeInsets.symmetric(horizontal: 8);
+  static const symmetricVerticalPaddingMini = EdgeInsets.symmetric(vertical: 8);
+  static const symmetricHorizontalMargin = EdgeInsets.symmetric(horizontal: 16);
+  static const symmetricVerticalMargin = EdgeInsets.symmetric(vertical: 16);
+  static const symmetricHorizontalMarginMini = EdgeInsets.symmetric(horizontal: 8);
+  static const symmetricVerticalMarginMini = EdgeInsets.symmetric(vertical: 8);
 
   static const mainCardBorderRadius = RoundedRectangleBorder(
     borderRadius: BorderRadius.all(
