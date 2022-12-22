9096925e6e6d3c785f63d17e1cf8e2cc4bba31ea
jmapps <jmappsmuslim@gmail.com>
2022-12-22 10:37:41 +0300

    Added styles

--- lib/application/style/app_styles.dart
+++ lib/application/style/app_styles.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AppStyles {
+
   static const mainFontSizeStyle = TextStyle(fontSize: 18);
+
+  static const mainPadding = EdgeInsets.all(16);
+  static const mainPaddingMini = EdgeInsets.all(8);
+  static const mainMargin = EdgeInsets.all(16);
+  static const mainMarginMini = EdgeInsets.all(8);
+
+  static const mainCardBorderRadius = RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(25),
+    ),
+  );
+
+  static const mainBorderRadius = BorderRadius.all(
+    Radius.circular(25),
+  );
 }
