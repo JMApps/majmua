0dda52d7146bd7d54a648598b02b04c4f475a7dc
jmapps <jmappsmuslim@gmail.com>
2022-12-25 07:42:54 +0300

    New order

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:majmua/application/strings/app_strings.dart';
 import 'package:majmua/application/style/app_styles.dart';
-import 'package:majmua/presentation/beforeHolidays/before_holidays_container.dart';
+import 'package:majmua/presentation/books/books_container.dart';
 import 'package:majmua/presentation/currentDate/current_date_container.dart';
 import 'package:majmua/presentation/friday/friday_container.dart';
 import 'package:majmua/presentation/prayerTime/prayer_time_container.dart';
+import 'package:majmua/presentation/restHolidays/rest_holidays.dart';
 import 'package:majmua/presentation/restTime/rest_time_container.dart';
 import 'package:majmua/presentation/sixColumns/six_column_container.dart';
 import 'package:majmua/presentation/whiteDays/white_day_container.dart';
@@ -26,21 +27,20 @@ class MainPage extends StatelessWidget {
             RestTimeContainer(),
             SizedBox(height: 6),
             Divider(indent: 16, endIndent: 16),
-            SizedBox(height: 6),
-            CurrentDateContainer(),
-            SizedBox(height: 8),
             PrayerTimeContainer(),
+            SizedBox(height: 4),
+            CurrentDateContainer(),
             SizedBox(height: 4),
             Divider(indent: 16, endIndent: 16),
             FridayContainer(),
-            SizedBox(height: 4),
-            BeforeHolidaysContainer(),
-            SizedBox(height: 6),
+            RestHolidaysContainer(),
             Divider(indent: 16, endIndent: 16),
             WhiteDayContainer(),
             SizedBox(height: 4),
             SixColumnsContainer(),
-            SizedBox(height: 6),
+            SizedBox(height: 4),
+            Divider(indent: 16, endIndent: 16),
+            BooksContainer(),
             Divider(indent: 16, endIndent: 16),
           ],
         ),
