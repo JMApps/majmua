1aa6453d3e77b88ad580f5323436d81efb17a480
jmapps <jmappsmuslim@gmail.com>
2022-12-29 18:05:40 +0300

    Added book 200 questions

---
+++ lib/presentation/library/questions/question_card.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:majmua/application/style/app_styles.dart';
+import 'package:majmua/application/theme/app_themes.dart';
+import 'package:majmua/data/database/local/model/question_model.dart';
+
+class QuestionItem extends StatelessWidget {
+  const  QuestionItem({Key? key, required this.item}) : super(key: key);
+
+  final QuestionModel item;
+
+  @override
+  Widget build(BuildContext context) {
+    final appColors = Theme.of(context).colorScheme;
+    return CupertinoScrollbar(
+      child: SingleChildScrollView(
+        padding: AppStyles.mainPadding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Divider(indent: 16, endIndent: 16),
+            Text(
+              'Вопрос ${item.id}',
+              style: const TextStyle(
+                fontSize: 18,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const Divider(indent: 16, endIndent: 16),
+            const SizedBox(height: 8),
+            Html(
+              data: item.question,
+              style: {
+                '#': Style(
+                  padding: EdgeInsets.zero,
+                  margin: EdgeInsets.zero,
+                  fontSize: const FontSize(18),
+                  color: appColors.firstAppColor,
+                  textAlign: TextAlign.center,
+                  fontWeight: FontWeight.bold,
+                ),
+                'small': Style(
+                  padding: EdgeInsets.zero,
+                  margin: EdgeInsets.zero,
+                  fontSize: const FontSize(12),
+                ),
+                'a': Style(
+                  padding: EdgeInsets.zero,
+                  margin: EdgeInsets.zero,
+                  fontSize: const FontSize(18),
+                  color: appColors.thirdAppColor,
+                ),
+              },
+              onLinkTap: (String? url, RenderContext rendContext,
+                  Map<String, String> attributes, element) {
+                showModalBottomSheet(
+                  backgroundColor: Colors.transparent,
+                  context: context,
+                  builder: (BuildContext context) => CupertinoActionSheet(
+                    message: Html(
+                      data: url,
+                      style: {
+                        '#': Style(
+                          padding: EdgeInsets.zero,
+                          margin: EdgeInsets.zero,
+                          fontSize: const FontSize(18),
+                        ),
+                        'small': Style(
+                          padding: EdgeInsets.zero,
+                          margin: EdgeInsets.zero,
+                          color: Colors.grey,
+                          fontSize: const FontSize(12),
+                        ),
+                      },
+                    ),
+                    actions: [
+                      CupertinoButton(
+                        child: Text(
+                          'Закрыть',
+                          style: TextStyle(
+                            color: appColors.thirdAppColor,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 8),
+            const Divider(indent: 16, endIndent: 16),
+            const Text(
+              'Ответ',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const Divider(indent: 16, endIndent: 16),
+            Html(
+              data: item.answerContent,
+              style: {
+                '#': Style(
+                  padding: EdgeInsets.zero,
+                  margin: EdgeInsets.zero,
+                  fontSize: const FontSize(18),
+                ),
+                'small': Style(
+                  padding: EdgeInsets.zero,
+                  margin: EdgeInsets.zero,
+                  fontSize: const FontSize(12),
+                ),
+                'a': Style(
+                  padding: EdgeInsets.zero,
+                  margin: EdgeInsets.zero,
+                  fontSize: const FontSize(18),
+                  color: appColors.thirdAppColor,
+                ),
+              },
+              onLinkTap: (String? url, RenderContext rendContext,
+                  Map<String, String> attributes, element) {
+                showModalBottomSheet(
+                  backgroundColor: Colors.transparent,
+                  context: context,
+                  builder: (BuildContext context) => CupertinoActionSheet(
+                    message: Html(
+                      data: url,
+                      style: {
+                        '#': Style(
+                          padding: EdgeInsets.zero,
+                          margin: EdgeInsets.zero,
+                          fontSize: const FontSize(18),
+                        ),
+                        'small': Style(
+                          padding: EdgeInsets.zero,
+                          margin: EdgeInsets.zero,
+                          color: Colors.grey,
+                          fontSize: const FontSize(12),
+                        ),
+                      },
+                    ),
+                    actions: [
+                      CupertinoButton(
+                        child: Text(
+                          'Закрыть',
+                          style: TextStyle(
+                            color: appColors.thirdAppColor,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/library/questions/questions_page.dart
+++ lib/presentation/library/questions/questions_page.dart
@@ -1,15 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:majmua/application/state/questions_pages_scroll_state.dart';
+import 'package:majmua/data/database/local/service/questions_query.dart';
+import 'package:majmua/presentation/library/questions/question_card.dart';
+import 'package:majmua/presentation/library/questions/questions_smooth_indicator.dart';
+import 'package:provider/provider.dart';
 
-class QuestionsPage extends StatelessWidget {
+class QuestionsPage extends StatefulWidget {
   const QuestionsPage({Key? key}) : super(key: key);
 
   @override
+  State<QuestionsPage> createState() => _QuestionsPageState();
+}
+
+class _QuestionsPageState extends State<QuestionsPage> {
+  final QuestionsQuery _questionsQuery = QuestionsQuery();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('200 вопросов'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<QuestionsPagedScrollState>(
+          create: (_) => QuestionsPagedScrollState(),
+        ),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('200 вопросов'),
+        ),
+        body: Consumer<QuestionsPagedScrollState>(
+          builder: (BuildContext context, scrollState, _) {
+            return FutureBuilder(
+              future: _questionsQuery.getAllQuestions(),
+              builder: (BuildContext context, snapshot) {
+                return snapshot.hasData
+                    ? Column(
+                        children: [
+                          Expanded(
+                            child: PageView.builder(
+                              controller: scrollState.getPageController,
+                              itemCount: snapshot.data!.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return QuestionItem(
+                                  item: snapshot.data![index],
+                                );
+                              },
+                            ),
+                          ),
+                          QuestionsSmoothIndicator(
+                            pageController: scrollState.getPageController,
+                            listLength: snapshot.data!.length,
+                          ),
+                          const SizedBox(height: 16),
+                        ],
+                      )
+                    : const Center(
+                        child: CircularProgressIndicator.adaptive(),
+                      );
+              },
+            );
+          },
+        ),
       ),
-      body: Container(),
     );
   }
 }
