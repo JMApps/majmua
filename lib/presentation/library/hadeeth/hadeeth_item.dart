1fd5a9586d4408ce2c7c44fb63a6ac578ab33b8b
jmapps <jmappsmuslim@gmail.com>
2022-12-29 18:38:01 +0300

    Added book 40 hadeeth

---
+++ lib/presentation/library/hadeeth/hadeeth_item.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:majmua/application/style/app_styles.dart';
+import 'package:majmua/application/theme/app_themes.dart';
+import 'package:majmua/data/database/local/model/hadeeth_model.dart';
+
+class HadeethItem extends StatelessWidget {
+  const HadeethItem({Key? key, required this.item}) : super(key: key);
+
+  final HadeethModel item;
+
+  @override
+  Widget build(BuildContext context) {
+    final appColors = Theme.of(context).colorScheme;
+    return CupertinoScrollbar(
+      child: SingleChildScrollView(
+        padding: AppStyles.mainPadding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Divider(indent: 16, endIndent: 16),
+            Text(
+              item.numberHadeeth,
+              style: const TextStyle(
+                fontSize: 18,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const Divider(indent: 16, endIndent: 16),
+            const SizedBox(height: 8),
+            Text(
+              item.titleHadeeth,
+              style: TextStyle(
+                fontSize: 18,
+                color: appColors.firstAppColor,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const Divider(indent: 16, endIndent: 16),
+            const SizedBox(height: 8),
+            Html(
+              data: item.hadeethArabic,
+              style: {
+                '#': Style(
+                    padding: EdgeInsets.zero,
+                    margin: EdgeInsets.zero,
+                    fontSize: const FontSize(20),
+                    direction: TextDirection.rtl,
+                    lineHeight: const LineHeight(2),
+                    fontFamily: 'Scheherazade'),
+                'small': Style(
+                    padding: EdgeInsets.zero,
+                    margin: EdgeInsets.zero,
+                    fontSize: const FontSize(12),
+                    direction: TextDirection.rtl,
+                    lineHeight: const LineHeight(2),
+                    fontFamily: 'Scheherazade'),
+              },
+            ),
+            const Divider(indent: 16, endIndent: 16),
+            const SizedBox(height: 8),
+            Html(
+              data: item.hadeethTranslation,
+              style: {
+                '#': Style(
+                  padding: EdgeInsets.zero,
+                  margin: EdgeInsets.zero,
+                  fontSize: const FontSize(18),
+                  direction: TextDirection.ltr,
+                ),
+                'small': Style(
+                  padding: EdgeInsets.zero,
+                  margin: EdgeInsets.zero,
+                  fontSize: const FontSize(12),
+                  direction: TextDirection.ltr,
+                ),
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/library/hadeeth/hadeeth_page.dart
+++ lib/presentation/library/hadeeth/hadeeth_page.dart
@@ -1,15 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:majmua/application/state/pages_scroll_state.dart';
+import 'package:majmua/data/database/local/service/hadeeth_query.dart';
+import 'package:majmua/presentation/library/hadeeth/hadeeth_item.dart';
+import 'package:majmua/presentation/library/hadeeth/hadeeth_smooth_indicator.dart';
+import 'package:provider/provider.dart';
 
-class HadeethPage extends StatelessWidget {
+class HadeethPage extends StatefulWidget {
   const HadeethPage({Key? key}) : super(key: key);
 
   @override
+  State<HadeethPage> createState() => _HadeethPageState();
+}
+
+class _HadeethPageState extends State<HadeethPage> {
+  final HadeethQuery _hadeethQuery = HadeethQuery();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('40 хадисов'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<PagesScrollState>(
+          create: (_) => PagesScrollState(),
+        ),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('40 хадисов'),
+        ),
+        body: Consumer<PagesScrollState>(
+          builder: (BuildContext context, scrollState, _) {
+            return FutureBuilder(
+              future: _hadeethQuery.getAllHadeeth(),
+              builder: (BuildContext context, snapshot) {
+                return snapshot.hasData
+                    ? Column(
+                        children: [
+                          Expanded(
+                            child: PageView.builder(
+                              controller: scrollState.getPageController,
+                              itemCount: snapshot.data!.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return HadeethItem(
+                                  item: snapshot.data![index],
+                                );
+                              },
+                            ),
+                          ),
+                          HadeethSmoothIndicator(
+                            pageController: scrollState.getPageController,
+                            listLength: snapshot.data!.length,
+                          ),
+                          const SizedBox(height: 16),
+                        ],
+                      )
+                    : const Center(
+                        child: CircularProgressIndicator.adaptive(),
+                      );
+              },
+            );
+          },
+        ),
       ),
-      body: Container(),
     );
   }
 }
