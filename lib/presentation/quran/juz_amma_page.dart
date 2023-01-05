8636baecec18713811d442faae93963b17046e3f
jmapps <jmappsmuslim@gmail.com>
2023-01-05 21:09:47 +0300

    Added safe area for horizontal orientation

--- lib/presentation/quran/juz_amma_page.dart
+++ lib/presentation/quran/juz_amma_page.dart
@@ -52,65 +52,69 @@ class _JuzAmmaPageState extends State<JuzAmmaPage> {
           ),
         ),
       ],
-      child: Scaffold(
-        backgroundColor: appColors.backgroundColorQ,
-        body: NestedScrollView(
-          floatHeaderSlivers: true,
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return [
-              SliverAppBar(
-                iconTheme: IconThemeData(
-                  color: appColors.mainTextColor,
-                ),
-                backgroundColor: Colors.transparent,
-                centerTitle: true,
-                elevation: 0,
-                floating: true,
-                snap: true,
-                forceElevated: innerBoxIsScrolled,
-                expandedHeight: 75,
-                title: Text(
-                  '30 джуз',
-                  style: TextStyle(
+      child: SafeArea(
+        top: false,
+        bottom: false,
+        child: Scaffold(
+          backgroundColor: appColors.backgroundColorQ,
+          body: NestedScrollView(
+            floatHeaderSlivers: true,
+            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+              return [
+                SliverAppBar(
+                  iconTheme: IconThemeData(
                     color: appColors.mainTextColor,
                   ),
+                  backgroundColor: Colors.transparent,
+                  centerTitle: true,
+                  elevation: 0,
+                  floating: true,
+                  snap: true,
+                  forceElevated: innerBoxIsScrolled,
+                  expandedHeight: 75,
+                  title: Text(
+                    '30 джуз',
+                    style: TextStyle(
+                      color: appColors.mainTextColor,
+                    ),
+                  ),
                 ),
-              ),
-            ];
-          },
-          body: Consumer<BookPagesScrollState>(
-            builder: (BuildContext context, scrollState, _) {
-              return PageView.builder(
-                controller: scrollState.getPageController,
-                reverse: true,
-                itemCount: _quranPageNames.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Center(
-                          child: SurahItem(
-                            surahPageName: _quranPageNames[index],
+              ];
+            },
+            body: Consumer<BookPagesScrollState>(
+              builder: (BuildContext context, scrollState, _) {
+                return PageView.builder(
+                  controller: scrollState.getPageController,
+                  reverse: true,
+                  itemCount: _quranPageNames.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Center(
+                            child: SurahItem(
+                              surahPageName: _quranPageNames[index],
+                            ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: AppStyles.symmetricVerticalPadding,
-                        child: Text(
-                          '${index + 1}',
-                          textAlign: TextAlign.center,
+                        Padding(
+                          padding: AppStyles.symmetricVerticalPadding,
+                          child: Text(
+                            '${index + 1}',
+                            textAlign: TextAlign.center,
+                          ),
                         ),
-                      ),
-                    ],
-                  );
-                },
-                onPageChanged: (page) {
-                  scrollState.changePageForLast(currentPage: page);
-                },
-              );
-            },
+                      ],
+                    );
+                  },
+                  onPageChanged: (page) {
+                    scrollState.changePageForLast(currentPage: page);
+                  },
+                );
+              },
+            ),
           ),
         ),
       ),
--- lib/presentation/quran/surah_sajdah_page.dart
+++ lib/presentation/quran/surah_sajdah_page.dart
@@ -21,67 +21,71 @@ class _SurahSajdahPageState extends State<SurahSajdahPage> {
   @override
   Widget build(BuildContext context) {
     final appColors = Theme.of(context).colorScheme;
-    return Scaffold(
-      backgroundColor: appColors.backgroundColorQ,
-      body: NestedScrollView(
-        floatHeaderSlivers: true,
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return [
-            SliverAppBar(
-              iconTheme: IconThemeData(
-                color: appColors.mainTextColor,
-              ),
-              backgroundColor: Colors.transparent,
-              centerTitle: true,
-              elevation: 0,
-              floating: true,
-              snap: true,
-              forceElevated: innerBoxIsScrolled,
-              expandedHeight: 75,
-              title: Text(
-                'Сура «Земной поклон»',
-                style: TextStyle(
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Scaffold(
+        backgroundColor: appColors.backgroundColorQ,
+        body: NestedScrollView(
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                iconTheme: IconThemeData(
                   color: appColors.mainTextColor,
                 ),
-              ),
-              actions: [
-                IconButton(
-                  onPressed: () {},
-                  splashRadius: 20,
-                  icon: Icon(
-                    CupertinoIcons.globe,
+                backgroundColor: Colors.transparent,
+                centerTitle: true,
+                elevation: 0,
+                floating: true,
+                snap: true,
+                forceElevated: innerBoxIsScrolled,
+                expandedHeight: 75,
+                title: Text(
+                  'Сура «Земной поклон»',
+                  style: TextStyle(
                     color: appColors.mainTextColor,
                   ),
                 ),
-              ],
-            ),
-          ];
-        },
-        body: PageView.builder(
-          reverse: true,
-          itemCount: _quranPageNames.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Center(
-                    child: SurahItem(
-                      surahPageName: _quranPageNames[index],
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    splashRadius: 20,
+                    icon: Icon(
+                      CupertinoIcons.globe,
+                      color: appColors.mainTextColor,
                     ),
                   ),
-                ),
-                Padding(
-                  padding: AppStyles.symmetricVerticalPadding,
-                  child: Text(
-                    '${index + 1}',
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ],
-            );
+                ],
+              ),
+            ];
           },
+          body: PageView.builder(
+            reverse: true,
+            itemCount: _quranPageNames.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: SurahItem(
+                        surahPageName: _quranPageNames[index],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: AppStyles.symmetricVerticalPadding,
+                    child: Text(
+                      '${index + 1}',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
