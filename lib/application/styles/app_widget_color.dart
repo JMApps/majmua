adc2e1aa6ed38881b801b795137b76f49f8093f4
jmapps <jmappsmuslim@gmail.com>
2023-01-12 08:59:38 +0300

    Added app widget color class

---
+++ lib/application/styles/app_widget_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppWidgetColor {
+  static const indigo = Color(0xFF5C6BC0);
+  static const red = Color(0xFFEF5350);
+  static const orange = Color(0xFFFFA726);
+  static const teal = Color(0xFF26A69A);
+  static const grey = Color(0xFF9E9E9E);
+}
