ac7a39940252f560d90c4a57fa17159818105c1a
jmapps <jmappsmuslim@gmail.com>
2023-01-15 12:29:02 +0300

    Removed elevation from card's

--- lib/presentation/prayerTime/item_card_adhan_name_time.dart
+++ lib/presentation/prayerTime/item_card_adhan_name_time.dart
@@ -17,39 +17,33 @@ class ItemCardAdhanNameTime extends StatelessWidget {
       child: Card(
         margin: const EdgeInsets.all(4),
         shape: AppWidgetStyle.mainRectangleBorderMini,
-        color: prayerColor,
-        child: Card(
-          elevation: 1,
-          margin: const EdgeInsets.only(left: 4),
-          shape: AppWidgetStyle.mainRectangleBorderMini,
-          child: Padding(
-            padding: AppWidgetStyle.mainPaddingMini,
-            child: Stack(
-              children: [
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Icon(
-                    Icons.notifications_off_outlined,
-                    color: Colors.teal.shade200,
-                    size: 17.5,
+        child: Padding(
+          padding: AppWidgetStyle.mainPaddingMini,
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: Icon(
+                  Icons.notifications_off_outlined,
+                  color: Colors.red.shade300,
+                  size: 17.5,
+                ),
+              ),
+              ListTile(
+                visualDensity: const VisualDensity(
+                  horizontal: 0,
+                  vertical: -4,
+                ),
+                contentPadding: EdgeInsets.zero,
+                title: Text(prayerName),
+                subtitle: const Text(
+                  '05:34',
+                  style: TextStyle(
+                    fontFamily: 'Lato',
                   ),
                 ),
-                ListTile(
-                  visualDensity: const VisualDensity(
-                    horizontal: 0,
-                    vertical: -4,
-                  ),
-                  contentPadding: EdgeInsets.zero,
-                  title: Text(prayerName),
-                  subtitle: const Text(
-                    '05:34',
-                    style: TextStyle(
-                      fontFamily: 'Lato',
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
