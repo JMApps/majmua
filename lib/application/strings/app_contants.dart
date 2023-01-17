e9c8e4775ea4726faf6fbd1e6a1cbdd4c40a52f0
jmapps <jmappsmuslim@gmail.com>
2023-01-17 11:42:54 +0300

    Added DB libraries with init Hive, app constants and transparent statusBarColor

---
+++ lib/application/strings/app_contants.dart
@@ -0,0 +1,25 @@
+class AppConstants {
+  static const keySettingsPrayerTimeBox = 'key_settings_prayer_time_box';
+  static const keyMainAppSettings = 'key_main_app_settings';
+
+  // Prayer time
+  static const keyCountryId = 'key_country_id';
+  static const keyCountry = 'key_country';
+  static const keyCity = 'key_city';
+  static const keyCurrentLatitude = 'key_current_latitude';
+  static const keyCurrentLongitude = 'key_current_longitude';
+  static const keyCalculationIndex = 'key_prayer_params_index';
+  static const keyMadhabIndex = 'key_madhab_params_index';
+  static const keyUtcOffsetIndex = 'key_utc_offset_index';
+
+  // Settings
+  static const keyNamesOfLastPage = 'key_names_of_last_page';
+  static const keyQuestionsLastPage = 'key_questions_last_page';
+  static const keyHadeethLastPage = 'key_hadeeth_last_page';
+  static const keyLessonsLastPage = 'key_lessons_last_page';
+  static const keyRaqaiqLastPage = 'key_raqaiq_last_page';
+  static const keyJuzAmmaLastPage = 'key_juz_amma_last_page';
+
+  static const keyMainBookTextSize = 'key_main_book_text_size';
+  static const keyMainBookTextColor = 'key_main_book_text_color';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:majmua/application/strings/app_contants.dart';
 import 'package:majmua/presentation/pages/root_page.dart';
 
-void main() {
+void main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
+  await Hive.initFlutter();
+  await Hive.openBox(AppConstants.keySettingsPrayerTimeBox);
+  await Hive.openBox(AppConstants.keyMainAppSettings);
   runApp(
     const RootPage(),
   );
