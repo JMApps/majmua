4746efd54c0ec5932e71cb6cba4709e72e02010d
jmapps <jmappsmuslim@gmail.com>
2023-01-27 21:52:57 +0300

    Added card's for settings

--- lib/presentation/settings/app_settings_page.dart
+++ lib/presentation/settings/app_settings_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:majmua/application/state/main_notifications_state.dart';
 import 'package:majmua/application/strings/app_strings.dart';
 import 'package:majmua/application/styles/app_widget_style.dart';
+import 'package:majmua/application/themes/app_theme.dart';
+import 'package:provider/provider.dart';
 
 class AppSettingsPage extends StatelessWidget {
   const AppSettingsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final ColorScheme appColors = Theme.of(context).colorScheme;
     return Scaffold(
       body: CustomScrollView(
         slivers: [
@@ -22,127 +26,155 @@ class AppSettingsPage extends StatelessWidget {
           SliverToBoxAdapter(
             child: Padding(
               padding: AppWidgetStyle.verticalPadding,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const Padding(
-                    padding: AppWidgetStyle.horizontalPadding,
-                    child: Text(
-                      AppString.notifications,
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
+              child: Consumer<MainNotificationsSate>(
+                builder: (BuildContext context, notificationState, _) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Padding(
+                        padding: AppWidgetStyle.horizontalPadding,
+                        child: Text(
+                          AppString.notifications,
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  SwitchListTile(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    title: const Text(AppString.dailyNotif),
-                    value: true,
-                    onChanged: (bool? value) {},
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  SwitchListTile(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    title: const Text(AppString.firstFastNotif),
-                    value: false,
-                    onChanged: (bool? value) {},
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  SwitchListTile(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    title: const Text(AppString.secondFastNotif),
-                    value: false,
-                    onChanged: (bool? value) {},
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  SwitchListTile(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    title: const Text(AppString.whiteDaysNotif),
-                    value: false,
-                    onChanged: (bool? value) {},
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  const Padding(
-                    padding: AppWidgetStyle.horizontalPadding,
-                    child: Text(
-                      AppString.prayersNotification,
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
+                      Card(
+                        margin: AppWidgetStyle.mainMargin,
+                        color: appColors.mainReverse,
+                        child: Wrap(
+                          children: [
+                            const Divider(indent: 16, endIndent: 16),
+                            SwitchListTile(
+                              visualDensity: const VisualDensity(vertical: -4),
+                              title: const Text(AppString.dailyNotif),
+                              value: true,
+                              onChanged: (bool? value) {},
+                            ),
+                            const Divider(indent: 16, endIndent: 16),
+                            SwitchListTile(
+                              visualDensity: const VisualDensity(vertical: -4),
+                              title: const Text(AppString.firstFastNotif),
+                              value: false,
+                              onChanged: (bool? value) {},
+                            ),
+                            const Divider(indent: 16, endIndent: 16),
+                            SwitchListTile(
+                              visualDensity: const VisualDensity(vertical: -4),
+                              title: const Text(AppString.secondFastNotif),
+                              value: false,
+                              onChanged: (bool? value) {},
+                            ),
+                            const Divider(indent: 16, endIndent: 16),
+                            SwitchListTile(
+                              visualDensity: const VisualDensity(vertical: -4),
+                              title: const Text(AppString.whiteDaysNotif),
+                              value: false,
+                              onChanged: (bool? value) {},
+                            ),
+                            const Divider(indent: 16, endIndent: 16),
+                          ],
+                        ),
                       ),
-                    ),
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  SwitchListTile(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    title: const Text(AppString.fajr),
-                    value: false,
-                    onChanged: (bool? value) {},
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  SwitchListTile(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    title: const Text(AppString.sunrise),
-                    value: false,
-                    onChanged: (bool? value) {},
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  SwitchListTile(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    title: const Text(AppString.dhuhr),
-                    value: false,
-                    onChanged: (bool? value) {},
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  SwitchListTile(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    title: const Text(AppString.asr),
-                    value: false,
-                    onChanged: (bool? value) {},
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  SwitchListTile(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    title: const Text(AppString.maghrib),
-                    value: false,
-                    onChanged: (bool? value) {},
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  SwitchListTile(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    title: const Text(AppString.isha),
-                    value: false,
-                    onChanged: (bool? value) {},
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  const Padding(
-                    padding: EdgeInsets.only(right: 16, top: 16, left: 16),
-                    child: Text(
-                      AppString.appTheme,
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
+                      const Padding(
+                        padding: AppWidgetStyle.horizontalPadding,
+                        child: Text(
+                          AppString.prayersNotification,
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  SwitchListTile(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    title: const Text(AppString.adaptiveTheme),
-                    value: true,
-                    onChanged: (bool? value) {},
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                  SwitchListTile(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    title: const Text(AppString.userTheme),
-                    value: false,
-                    onChanged: (bool? value) {},
-                  ),
-                  const Divider(indent: 16, endIndent: 16),
-                ],
+                      Card(
+                        margin: AppWidgetStyle.mainMargin,
+                        color: appColors.mainReverse,
+                        child: Wrap(
+                          children: [
+                            const Divider(indent: 16, endIndent: 16),
+                            SwitchListTile(
+                              visualDensity: const VisualDensity(vertical: -4),
+                              title: const Text(AppString.fajr),
+                              value: false,
+                              onChanged: (bool? value) {},
+                            ),
+                            const Divider(indent: 16, endIndent: 16),
+                            SwitchListTile(
+                              visualDensity: const VisualDensity(vertical: -4),
+                              title: const Text(AppString.sunrise),
+                              value: false,
+                              onChanged: (bool? value) {},
+                            ),
+                            const Divider(indent: 16, endIndent: 16),
+                            SwitchListTile(
+                              visualDensity: const VisualDensity(vertical: -4),
+                              title: const Text(AppString.dhuhr),
+                              value: false,
+                              onChanged: (bool? value) {},
+                            ),
+                            const Divider(indent: 16, endIndent: 16),
+                            SwitchListTile(
+                              visualDensity: const VisualDensity(vertical: -4),
+                              title: const Text(AppString.asr),
+                              value: false,
+                              onChanged: (bool? value) {},
+                            ),
+                            const Divider(indent: 16, endIndent: 16),
+                            SwitchListTile(
+                              visualDensity: const VisualDensity(vertical: -4),
+                              title: const Text(AppString.maghrib),
+                              value: false,
+                              onChanged: (bool? value) {},
+                            ),
+                            const Divider(indent: 16, endIndent: 16),
+                            SwitchListTile(
+                              visualDensity: const VisualDensity(vertical: -4),
+                              title: const Text(AppString.isha),
+                              value: false,
+                              onChanged: (bool? value) {},
+                            ),
+                            const Divider(indent: 16, endIndent: 16),
+                          ],
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(right: 16, top: 16, left: 16),
+                        child: Text(
+                          AppString.appTheme,
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Card(
+                        margin: AppWidgetStyle.mainMargin,
+                        color: appColors.mainReverse,
+                        child: Wrap(
+                          children: [
+                            const Divider(indent: 16, endIndent: 16),
+                            SwitchListTile(
+                              visualDensity: const VisualDensity(vertical: -4),
+                              title: const Text(AppString.adaptiveTheme),
+                              value: true,
+                              onChanged: (bool? value) {},
+                            ),
+                            const Divider(indent: 16, endIndent: 16),
+                            SwitchListTile(
+                              visualDensity: const VisualDensity(vertical: -4),
+                              title: const Text(AppString.userTheme),
+                              value: false,
+                              onChanged: (bool? value) {},
+                            ),
+                            const Divider(indent: 16, endIndent: 16),
+                          ],
+                        ),
+                      )
+                    ],
+                  );
+                },
               ),
             ),
           ),
