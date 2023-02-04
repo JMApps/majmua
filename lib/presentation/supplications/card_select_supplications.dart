16fee8709921681f516aa31402eeae1d1c7a6eaa
jmapps <jmappsmuslim@gmail.com>
2023-02-04 19:46:46 +0300

    Added supplication card with column's

---
+++ lib/presentation/supplications/card_select_supplications.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:majmua/application/strings/app_strings.dart';
+import 'package:majmua/application/styles/app_widget_style.dart';
+import 'package:majmua/application/themes/app_theme.dart';
+import 'package:majmua/presentation/supplications/column_supplications.dart';
+
+class CardSelectSupplications extends StatelessWidget {
+  const CardSelectSupplications({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme appColors = Theme.of(context).colorScheme;
+    return Card(
+      child: InkWell(
+        onTap: () {
+          showModalBottomSheet(
+            context: context,
+            backgroundColor: Colors.transparent,
+            builder: (_) => const ColumnSupplications(),
+          );
+        },
+        borderRadius: AppWidgetStyle.mainBorderRadius,
+        child: Card(
+          margin: AppWidgetStyle.mainMarginMini,
+          color: appColors.glassOnGlassCardColor,
+          child: const Padding(
+            padding: AppWidgetStyle.mainPaddingMini,
+            child: Text(
+              AppString.supplications,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/supplications/column_supplications.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:majmua/application/strings/app_strings.dart';
+import 'package:majmua/application/styles/app_widget_style.dart';
+import 'package:majmua/application/themes/app_theme.dart';
+import 'package:majmua/presentation/supplications/item_supplication_column.dart';
+
+class ColumnSupplications extends StatelessWidget {
+  const ColumnSupplications({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme appColors = Theme.of(context).colorScheme;
+    return Card(
+      margin: AppWidgetStyle.mainMargin,
+      color: appColors.mainReverse,
+      child: Padding(
+        padding: AppWidgetStyle.mainPadding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            ItemSupplicationColumn(
+              title: AppString.supplicationAfterPrayer,
+              icon: CupertinoIcons.hand_raised,
+              columnIndex: 0,
+            ),
+            Divider(indent: 16, endIndent: 16),
+            ItemSupplicationColumn(
+              title: AppString.supplicationMorning,
+              icon: CupertinoIcons.sunrise,
+              columnIndex: 1,
+            ),
+            Divider(indent: 16, endIndent: 16),
+            ItemSupplicationColumn(
+              title: AppString.supplicationEvening,
+              icon: CupertinoIcons.sunset,
+              columnIndex: 2,
+            ),
+            Divider(indent: 16, endIndent: 16),
+            ItemSupplicationColumn(
+              title: AppString.supplicationNight,
+              icon: CupertinoIcons.moon,
+              columnIndex: 3,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
