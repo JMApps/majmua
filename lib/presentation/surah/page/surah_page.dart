7870ae2e5c2e2a312a03e1445f5111450ca0c568
jmapps <jmappsmuslim@gmail.com>
2023-02-11 22:16:14 +0300

    Added surah page

---
+++ lib/presentation/surah/page/surah_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SurahPage extends StatelessWidget {
+  const SurahPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
