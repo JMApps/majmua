43486fb77d2eaab96c057873cd2504f1fee712fa
dev_muslim <jmappsmuslim@gmail.com>
2023-11-09 11:23:41 +0300

    Added initializations

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:majmua/core/strings/app_constraints.dart';
 import 'package:majmua/presentation/pages/root_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isAndroid) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+      ),
+    );
+  }
+  await Hive.initFlutter();
+  await Hive.openBox(AppConstraints.keyMainSettingBox);
   runApp(
     const RootPage(),
   );
