aa61032c453e495f0fb91178f7b72bfdb052a534
dev_muslim <jmappsmuslim@gmail.com>
2023-11-09 11:23:07 +0300

    Added localization

--- lib/presentation/pages/root_page.dart
+++ lib/presentation/pages/root_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:majmua/core/themes/app_themes.dart';
 import 'package:majmua/presentation/pages/main_page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({super.key});
@@ -9,6 +10,9 @@ class RootPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      onGenerateTitle: (context) => AppLocalizations.of(context)!.appName,
       theme: AppThemes.lightTheme,
       darkTheme: AppThemes.darkTheme,
       home: const MainPage(),
