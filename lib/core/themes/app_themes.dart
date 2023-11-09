412a8468841a74fa70a0e9ce98699f1e625cbca1
dev_muslim <jmappsmuslim@gmail.com>
2023-11-09 11:22:33 +0300

    Added fonts

--- lib/core/themes/app_themes.dart
+++ lib/core/themes/app_themes.dart
@@ -1,21 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:majmua/core/styles/app_styles.dart';
 
 class AppThemes {
   static ThemeData lightTheme = ThemeData(
-    brightness: Brightness.light,
-    colorSchemeSeed: const Color(0xFF3F51B5),
+    colorScheme: ColorScheme.fromSeed(
+      brightness: Brightness.light,
+      seedColor: const Color(0xFF3F51B5),
+      secondary: const Color(0xFF009688),
+      tertiary: const Color(0xFFFF9800),
+    ),
+    fontFamily: 'Next',
     appBarTheme: const AppBarTheme(
+      shape: AppStyles.bottomShape,
       centerTitle: true,
       elevation: 0,
+      titleTextStyle: TextStyle(
+        fontFamily: 'Nexa',
+        fontSize: 20,
+      ),
     ),
   );
 
   static ThemeData darkTheme = ThemeData(
-    brightness: Brightness.dark,
-    colorSchemeSeed: const Color(0xFF3F51B5),
+    colorScheme: ColorScheme.fromSeed(
+      brightness: Brightness.dark,
+      seedColor: const Color(0xFF3F51B5),
+      secondary: const Color(0xFF009688),
+      tertiary: const Color(0xFFFF9800),
+    ),
+    fontFamily: 'Next',
     appBarTheme: const AppBarTheme(
+      shape: AppStyles.bottomShape,
       centerTitle: true,
       elevation: 0,
+      titleTextStyle: TextStyle(
+        fontFamily: 'Nexa',
+        fontSize: 20,
+      ),
     ),
   );
 }
