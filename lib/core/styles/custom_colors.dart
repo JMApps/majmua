6b7dad146485fddfe64dad9e2b713d2d18936297
dev_muslim <jmappsmuslim@gmail.com>
2023-11-12 08:33:31 +0300

    Added more colors

--- lib/core/styles/custom_colors.dart
+++ lib/core/styles/custom_colors.dart
@@ -3,5 +3,9 @@ import 'package:flutter/material.dart';
 class CustomColors {
   static const Color indigo = Color(0xFF3F51B5);
   static const Color teal = Color(0xFF009688);
+  static const Color red = Color(0xFFF44336);
   static const Color orange = Color(0xFFFF9800);
+
+  static const Color cardLightColor = Color(0xCCE8EAF6);
+  static const Color cardDarkColor = Color(0xCC171821);
 }
--- lib/core/themes/app_themes.dart
+++ lib/core/themes/app_themes.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:majmua/core/styles/app_styles.dart';
+import 'package:majmua/core/styles/custom_colors.dart';
 
 class AppThemes {
   static ThemeData lightTheme = ThemeData(
     colorScheme: ColorScheme.fromSeed(
       brightness: Brightness.light,
-      seedColor: const Color(0xFF3F51B5),
-      secondary: const Color(0xFF009688),
-      tertiary: const Color(0xFFFF9800),
+      seedColor: CustomColors.indigo,
+      secondary: CustomColors.orange,
+      tertiary: CustomColors.teal,
     ),
     fontFamily: 'Next',
     appBarTheme: const AppBarTheme(
@@ -18,15 +19,20 @@ class AppThemes {
         fontFamily: 'Nexa',
         fontSize: 20,
       ),
+    ),
+    cardTheme: const CardTheme(
+      shape: AppStyles.mainShape,
+      elevation: 0,
+      color: CustomColors.cardLightColor,
     ),
   );
 
   static ThemeData darkTheme = ThemeData(
     colorScheme: ColorScheme.fromSeed(
       brightness: Brightness.dark,
-      seedColor: const Color(0xFF3F51B5),
-      secondary: const Color(0xFF009688),
-      tertiary: const Color(0xFFFF9800),
+      seedColor: CustomColors.indigo,
+      secondary: CustomColors.orange,
+      tertiary: CustomColors.teal,
     ),
     fontFamily: 'Next',
     appBarTheme: const AppBarTheme(
@@ -38,5 +44,36 @@ class AppThemes {
         fontSize: 20,
       ),
     ),
+    cardTheme: const CardTheme(
+      shape: AppStyles.mainShape,
+      elevation: 0,
+      color: CustomColors.cardDarkColor,
+    ),
   );
+}
+
+extension ColorSchemeS on ColorScheme {
+  Color get cardColor => brightness == Brightness.light
+      ? CustomColors.cardLightColor
+      : CustomColors.cardDarkColor;
+
+  Color get glass => brightness == Brightness.light
+      ? const Color(0xE6FFFFFF)
+      : const Color(0xE621222F);
+
+  Color get springColor => brightness == Brightness.light
+      ? const Color(0xFF43A047)
+      : const Color(0xD91B5E20);
+
+  Color get summerColor => brightness == Brightness.light
+      ? const Color(0xFF00897B)
+      : const Color(0xD9004D40);
+
+  Color get fallColor => brightness == Brightness.light
+      ? const Color(0xFFFB8C00)
+      : const Color(0xD9E65100);
+
+  Color get winterColor => brightness == Brightness.light
+      ? const Color(0xFF1E88E5)
+      : const Color(0xD90D47A1);
 }
