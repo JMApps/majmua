9ba025f6e90d76d62422d47f578bb39cb974f228
dev_muslim <jmappsmuslim@gmail.com>
2023-11-12 08:32:52 +0300

    Added main widget list

---
+++ lib/presentation/lists/main_widgets_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:majmua/core/styles/app_styles.dart';
+import 'package:majmua/presentation/restTimeSeasons/main_percent_seasons_card.dart';
+
+class MainWidgetsList extends StatelessWidget {
+  const MainWidgetsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      padding: AppStyles.mainMardingMicro,
+      child: Column(
+        children: [
+          MainPercentSeasonsCard(),
+        ],
+      ),
+    );
+  }
+}
