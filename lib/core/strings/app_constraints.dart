9a4eb17cc064c52eae397de33612600de3ebfe1c
dev_muslim <jmappsmuslim@gmail.com>
2023-11-17 09:20:28 +0300

    Added app constraint strings

--- lib/core/strings/app_constraints.dart
+++ lib/core/strings/app_constraints.dart
@@ -1,3 +1,68 @@
 class AppConstraints {
-  static const String keyMainSettingBox = 'key_main_settings_box';
+  static const keyMainSettings = 'key_main_settings';
+  static const keyWeeklySalawatSettings = 'key_weekly_salawat_settings';
+  static const keySettingsPrayerTimeBox = 'key_settings_prayer_time_box';
+  static const keyMainAppSettings = 'key_main_app_settings';
+  static const keyMainCounter = 'key_main_counter';
+
+  // Prayer time
+  static const keyCountryId = 'key_country_id';
+  static const keyCountry = 'key_country';
+  static const keyCity = 'key_city';
+  static const keyCurrentLatitude = 'key_current_latitude';
+  static const keyCurrentLongitude = 'key_current_longitude';
+  static const keyCalculationIndex = 'key_prayer_params_index';
+  static const keyMadhabIndex = 'key_madhab_params_index';
+  static const keyUtcOffsetIndex = 'key_utc_offset_index';
+
+  // Prayer adjustments
+  static const keyFajrAdjustment = 'key_fajr_adjustment';
+  static const keySunriseAdjustment = 'key_sunrise_adjustment';
+  static const keyDhuhrAdjustment = 'key_dhuhr_adjustment';
+  static const keyAsrAdjustment = 'key_asr_adjustment';
+  static const keyMaghribAdjustment = 'key_maghrib_adjustment';
+  static const keyIshaAdjustment = 'key_isha_adjustment';
+
+  // Notifications
+  static const keyFastMondayNotification = 'key_fast_monday_notification';
+  static const keyFastThursdayNotification = 'key_fast_thursday_notification';
+  static const keyFastWhiteDaysNotification = 'key_fast_white_days_notification';
+
+  static const keyFajrNotification = 'key_fajr_notification';
+  static const keySunriseNotification = 'key_sunrise_notification';
+  static const keyDhuhrNotification = 'key_dhuhr_notification';
+  static const keyAsrNotification = 'key_asr_notification';
+  static const keyMaghribNotification = 'key_maghrib_notification';
+  static const keyIshaNotification = 'key_isha_notification';
+
+  static const keyMorningSupplicationsNotification = 'key_morning_supplications_notification';
+  static const keyEveningSupplicationsNotification = 'key_evening_supplications_notification';
+
+  // Settings
+  static const keyNamesOfLastPage = 'key_names_of_last_page';
+  static const keyQuestionsLastPage = 'key_questions_last_page';
+  static const keyHadeethLastPage = 'key_hadeeth_last_page';
+  static const keyLessonsLastPage = 'key_lessons_last_page';
+  static const keyRaqaiqLastPage = 'key_raqaiq_last_page';
+  static const keyJuzAmmaLastPage = 'key_juz_amma_last_page';
+
+  static const keyMainBookTextSize = 'key_main_book_text_size';
+  static const keyMainBookTextColor = 'key_main_book_text_color';
+
+  static const keySalawatCount = 'key_salawat_count';
+
+  static const keyAdaptiveTheme = 'key_adaptive_theme';
+  static const keyUserTheme = 'key_user_theme';
+  static const keyWakeLock = 'key_wake_lock';
+
+  // Text's
+  static const keyMainTextSize = 'key_main_text_size';
+  static const keyIsTranscriptionShow = 'key_is_transcription_show';
+
+  // Counter
+  static const keyCounterValue = 'key_counter_value';
+  static const keyCounterAllValue = 'key_counter_all_value';
+  static const keyCounterVibrate = 'key_counter_vibrate';
+  static const keyCounterClick = 'key_counter_click';
+  static const keyCountValueShow = 'key_count_value_show';
 }
