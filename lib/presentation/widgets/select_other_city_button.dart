24910c4aed88fa89c0f64785a9125f93c41ae506
dev_muslim <jmappsmuslim@gmail.com>
2023-12-08 09:52:14 +0300

    Added select other city button

---
+++ lib/presentation/widgets/select_other_city_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:majmua/core/routes/route_names.dart';
+import 'package:majmua/core/themes/app_themes.dart';
+
+class SelectOtherCityButton extends StatelessWidget {
+  const SelectOtherCityButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final AppLocalizations? appLocale = AppLocalizations.of(context);
+    final ColorScheme appColors = Theme.of(context).colorScheme;
+    return FilledButton.tonal(
+      onPressed: () {
+        Navigator.of(context).pop();
+        Navigator.pushNamed(context, RouteNames.selectCityPage);
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(appColors.quaternaryColor.withOpacity(0.25)),
+      ),
+      child: Text(
+        appLocale!.selectOtherCity,
+        style: const TextStyle(
+          fontSize: 16,
+          fontFamily: 'Nexa',
+        ),
+      ),
+    );
+  }
+}
