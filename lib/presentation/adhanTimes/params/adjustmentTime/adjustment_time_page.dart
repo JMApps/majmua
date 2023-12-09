9e5b04fabef43bd7932b46c6dfc5808cfb2f293f
dev_muslim <jmappsmuslim@gmail.com>
2023-12-09 10:04:41 +0300

    Added change adjustment logic

--- lib/presentation/adhanTimes/params/adjustmentTime/adjustment_time_page.dart
+++ lib/presentation/adhanTimes/params/adjustmentTime/adjustment_time_page.dart
@@ -13,13 +13,13 @@ class AdjustmentTimePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final ColorScheme appColors = Theme.of(context).colorScheme;
     final AppLocalizations? appLocale = AppLocalizations.of(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(appLocale!.prayerTimeAdjustment),
-      ),
-      body: Consumer<AdhanTimeState>(
-        builder: (BuildContext context, AdhanTimeState adhanTimeState, _) {
-          return SingleChildScrollView(
+    return Consumer<AdhanTimeState>(
+      builder: (BuildContext context, AdhanTimeState adhanTimeState, _) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(appLocale!.prayerTimeAdjustment),
+          ),
+          body: SingleChildScrollView(
             padding: AppStyles.mainMardingMini,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -42,53 +42,65 @@ class AdjustmentTimePage extends StatelessWidget {
                 ),
                 AdjustmentItem(
                   prayerName: appLocale.fajr,
+                  prayerIndex: 1,
                   prayerTime: adhanTimeState.getPrayerTimes.fajr,
                 ),
                 const Divider(indent: 16, endIndent: 16),
                 AdjustmentItem(
                   prayerName: appLocale.sunrise,
+                  prayerIndex: 2,
                   prayerTime: adhanTimeState.getPrayerTimes.sunrise,
                 ),
                 const Divider(indent: 16, endIndent: 16),
                 AdjustmentItem(
                   prayerName: appLocale.dhuhr,
+                  prayerIndex: 3,
                   prayerTime: adhanTimeState.getPrayerTimes.dhuhr,
                 ),
                 const Divider(indent: 16, endIndent: 16),
                 AdjustmentItem(
                   prayerName: appLocale.asr,
+                  prayerIndex: 4,
                   prayerTime: adhanTimeState.getPrayerTimes.asr,
                 ),
                 const Divider(indent: 16, endIndent: 16),
                 AdjustmentItem(
                   prayerName: appLocale.maghrib,
+                  prayerIndex: 5,
                   prayerTime: adhanTimeState.getPrayerTimes.maghrib,
                 ),
                 const Divider(indent: 16, endIndent: 16),
                 AdjustmentItem(
                   prayerName: appLocale.isha,
+                  prayerIndex: 6,
                   prayerTime: adhanTimeState.getPrayerTimes.isha,
                 ),
               ],
             ),
-          );
-        },
-      ),
-      bottomNavigationBar: MaterialButton(
-        onPressed: () {
-          // Default adjustments
-        },
-        elevation: 0,
-        height: 50,
-        color: appColors.quaternaryColor,
-        shape: AppStyles.mainShapeMini,
-        child: Text(
-          appLocale.defaultVal,
-          style: const TextStyle(
-            fontSize: 18,
           ),
-        ),
-      ),
+          bottomNavigationBar: MaterialButton(
+            onPressed: () {
+              adhanTimeState.setFajrAdjustment = 0;
+              adhanTimeState.setSunriseAdjustment = 0;
+              adhanTimeState.setDhuhrAdjustment = 0;
+              adhanTimeState.setAsrAdjustment = 0;
+              adhanTimeState.setMaghribAdjustment = 0;
+              adhanTimeState.setIshaAdjustment = 0;
+              adhanTimeState.initPrayerTime();
+            },
+            elevation: 0,
+            height: 50,
+            color: appColors.quaternaryColor,
+            shape: AppStyles.mainShapeMini,
+            child: Text(
+              appLocale.defaultVal,
+              style: const TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
