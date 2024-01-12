51897e7beabe6fc41c9051b3a9d965369c912fb5
dev_muslim <jmappsmuslim@gmail.com>
2024-01-12 09:32:01 +0300

    Added text share button

--- lib/presentation/gems/gem_item.dart
+++ lib/presentation/gems/gem_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:html/parser.dart' show parse;
 import 'package:majmua/core/themes/app_themes.dart';
+import 'package:majmua/presentation/widgets/text_share_button.dart';
 import 'package:provider/provider.dart';
 import 'package:share_plus/share_plus.dart';
 
@@ -27,58 +28,36 @@ class GemItem extends StatelessWidget {
     return Card(
       margin: AppStyles.mardingWithoutBottomMini,
       color: appColors.glass,
-      child: InkWell(
-        onTap: () {
-          showModalBottomSheet(
-            context: context,
-            backgroundColor: appColors.fullGlass,
-            builder: (context) => Padding(
-              padding: AppStyles.mardingWithoutTop,
-              child: ListTile(
-                onTap: () {
-                  Navigator.pop(context);
-                  Share.share(
-                    '${parse(model.citation).body!.text}\n\nШейх ‘Абду-ль-‘Азиз ат-Тарифи',
-                    sharePositionOrigin: const Rect.fromLTWH(1, 1, 1, 2 / 2),
-                  );
-                },
-                tileColor: appColors.primary.withOpacity(0.25),
-                title: Text(appLocale!.share),
-                trailing: Image.asset(
-                  'assets/icons/share.png',
-                  width: 25,
-                  height: 25,
-                  color: appColors.primary,
+      child: Padding(
+        padding: AppStyles.mainMardingMini,
+        child: Consumer<GemsSettingsState>(
+          builder: (BuildContext context, GemsSettingsState gemsState, _) {
+            return Column(
+              children: [
+                FortressHtmlText(
+                  textData: model.citation,
+                  textSize: gemsState.getTextSize.toDouble(),
+                  textColor: appColors.inverseSurface,
+                  fontFamily: 'Gilroy',
+                  footnoteColor: appColors.quaternaryColor,
+                  textDataAlign: TextAlign.center,
                 ),
-              ),
-            ),
-          );
-        },
-        borderRadius: AppStyles.mainBorderRadiusMini,
-        child: Padding(
-          padding: AppStyles.mainMardingMini,
-          child: Consumer<GemsSettingsState>(
-            builder: (BuildContext context, GemsSettingsState gemsState, _) {
-              return Column(
-                children: [
-                  FortressHtmlText(
-                    textData: model.citation,
-                    textSize: gemsState.getTextSize.toDouble(),
-                    textColor: appColors.inverseSurface,
-                    fontFamily: 'Gilroy',
-                    footnoteColor: appColors.quaternaryColor,
-                    textDataAlign: TextAlign.center,
-                  ),
-                  Text(
-                    model.id.toString(),
-                    style: TextStyle(
-                      color: appColors.secondaryColor,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      model.id.toString(),
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: appColors.secondaryColor,
+                      ),
                     ),
-                  ),
-                ],
-              );
-            },
-          ),
+                    TextShareButton(contentForShare: '${parse(model.citation).body!.text}\n\nШейх ‘Абду-ль-‘Азиз ат-Тарифи')
+                  ],
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
--- lib/presentation/gems/gem_page_item.dart
+++ lib/presentation/gems/gem_page_item.dart
@@ -9,6 +9,7 @@ import '../../core/styles/app_styles.dart';
 import '../../domain/entities/gem_entity.dart';
 import '../fortress/fortress_html_text.dart';
 import '../state/gems_settings_state.dart';
+import '../widgets/text_share_button.dart';
 
 class GemPageItem extends StatelessWidget {
   const GemPageItem({
@@ -27,65 +28,42 @@ class GemPageItem extends StatelessWidget {
     return Card(
       margin: AppStyles.mardingWithoutBottomMini,
       color: appColors.glass,
-      child: InkWell(
-        onTap: () {
-          showModalBottomSheet(
-            context: context,
-            backgroundColor: appColors.fullGlass,
-            builder: (context) => Padding(
-              padding: AppStyles.mardingWithoutTop,
-              child: ListTile(
-                onTap: () {
-                  Navigator.pop(context);
-                  Share.share(
-                    '${parse(model.citation).body!.text}\n\nШейх ‘Абду-ль-‘Азиз ат-Тарифи',
-                    sharePositionOrigin: const Rect.fromLTWH(1, 1, 1, 2 / 2),
-                  );
-                },
-                tileColor: appColors.primary.withOpacity(0.25),
-                title: Text(appLocale!.share),
-                trailing: Image.asset(
-                  'assets/icons/share.png',
-                  width: 25,
-                  height: 25,
-                  color: appColors.primary,
-                ),
-              ),
-            ),
-          );
-        },
-        borderRadius: AppStyles.mainBorderRadiusMini,
-        child: Center(
-          child: SingleChildScrollView(
-            padding: AppStyles.mainMarding,
-            child: Consumer<GemsSettingsState>(
-              builder: (BuildContext context, GemsSettingsState gemsState, _) {
-                return Column(
-                  children: [
-                    FortressHtmlText(
-                      textData: model.citation,
-                      textSize: gemsState.getTextSize.toDouble(),
-                      textColor: appColors.inverseSurface,
-                      fontFamily: 'Gilroy',
-                      footnoteColor: appColors.quaternaryColor,
-                      textDataAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 8),
-                    CircleAvatar(
-                      backgroundColor: appColors.primary.withOpacity(0.05),
-                      radius: 20,
-                      child: Text(
-                        model.id.toString(),
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: appColors.inverseSurface,
+      child: Center(
+        child: SingleChildScrollView(
+          padding: AppStyles.mainMarding,
+          child: Consumer<GemsSettingsState>(
+            builder: (BuildContext context, GemsSettingsState gemsState, _) {
+              return Column(
+                children: [
+                  FortressHtmlText(
+                    textData: model.citation,
+                    textSize: gemsState.getTextSize.toDouble(),
+                    textColor: appColors.inverseSurface,
+                    fontFamily: 'Gilroy',
+                    footnoteColor: appColors.quaternaryColor,
+                    textDataAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: appColors.primary.withOpacity(0.25),
+                        radius: 20,
+                        child: Text(
+                          model.id.toString(),
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: appColors.inverseSurface,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                );
-              },
-            ),
+                      TextShareButton(contentForShare: '${parse(model.citation).body!.text}\n\nШейх ‘Абду-ль-‘Азиз ат-Тарифи')
+                    ],
+                  ),
+                ],
+              );
+            },
           ),
         ),
       ),
