0a575f70aa5c64f4ae22124f383fb418ce3a932a
dev_muslim <jmappsmuslim@gmail.com>
2024-03-05 19:59:57 +0300

    Small fixes

--- lib/presentation/surah/surahs_page.dart
+++ lib/presentation/surah/surahs_page.dart
@@ -64,95 +64,89 @@ class _SurahsPageState extends State<SurahsPage> with SingleTickerProviderStateM
           },
           child: Container(
             color: appColors.fullGlass,
-            child: SafeArea(
-              top: false,
-              bottom: false,
-              child: Scaffold(
-                backgroundColor: Colors.yellow.withOpacity(
-                  Theme.of(context).brightness == Brightness.light
-                      ? surahSettings.getWarmthValue / 100
-                      : surahSettings.getWarmthValue / 300,
-                ),
-                extendBody: true,
-                appBar: SlidingAppBar(
-                  controller: _controller,
-                  visible: surahSettings.getAppBarIsVisible,
-                  child: surahSettings.getAppBarIsVisible
-                      ? AppBar(
-                          leading: const UserBackButton(),
-                          title: Text(widget.surahName),
-                          actions: [
-                            IconButton(
-                              onPressed: () {
-                                _mainSettingsBox.put(widget.surahKey, 0);
-                                _surahPageController.animateToPage(
-                                  0,
-                                  duration: const Duration(milliseconds: 350),
-                                  curve: Curves.linear,
-                                );
-                              },
-                              icon: const Icon(
-                                CupertinoIcons.arrow_counterclockwise,
-                                color: Colors.white,
-                              ),
+            child: Scaffold(
+              backgroundColor: Colors.yellow.withOpacity(
+                Theme.of(context).brightness == Brightness.light
+                    ? surahSettings.getWarmthValue / 100
+                    : surahSettings.getWarmthValue / 300,
+              ),
+              extendBody: true,
+              appBar: SlidingAppBar(
+                controller: _controller,
+                visible: surahSettings.getAppBarIsVisible,
+                child: surahSettings.getAppBarIsVisible
+                    ? AppBar(
+                        leading: const UserBackButton(),
+                        title: Text(widget.surahName),
+                        actions: [
+                          IconButton(
+                            onPressed: () {
+                              _mainSettingsBox.put(widget.surahKey, 0);
+                              _surahPageController.animateToPage(
+                                0,
+                                duration: const Duration(milliseconds: 350),
+                                curve: Curves.linear,
+                              );
+                            },
+                            icon: const Icon(
+                              CupertinoIcons.arrow_counterclockwise,
+                              color: Colors.white,
                             ),
-                            IconButton(
-                              onPressed: () {
-                                showModalBottomSheet(
-                                  context: context,
-                                  backgroundColor: appColors.surface,
-                                  isScrollControlled: true,
-                                  builder: (context) {
-                                    return const SurahSettings();
-                                  },
-                                );
-                              },
-                              splashRadius: 20,
-                              tooltip: appLocale!.settings,
-                              icon: Image.asset(
-                                'assets/icons/setting.png',
-                                width: 25,
-                                height: 25,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        )
-                      : const PreferredSize(
-                          preferredSize: Size(0, 100),
-                          child: SizedBox(
-                            height: 100,
                           ),
-                        ),
-                ),
-                body: PageView.builder(
-                  controller: _surahPageController,
-                  reverse: true,
-                  itemCount: AppStrings.surahsLists[widget.surahNumber].length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final String surahPage = AppStrings.surahsLists[widget.surahNumber][index];
-                    return Center(
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            Image.asset(
-                              'assets/quran/$surahPage.png',
-                              fit: BoxFit.cover,
-                              color: appColors.inverseSurface.withOpacity(
-                                surahSettings.getFontContrast / 100,
-                              ),
+                          IconButton(
+                            onPressed: () {
+                              showModalBottomSheet(
+                                context: context,
+                                backgroundColor: appColors.surface,
+                                isScrollControlled: true,
+                                builder: (context) {
+                                  return const SurahSettings();
+                                },
+                              );
+                            },
+                            splashRadius: 20,
+                            tooltip: appLocale!.settings,
+                            icon: Image.asset(
+                              'assets/icons/setting.png',
+                              width: 25,
+                              height: 25,
+                              color: Colors.white,
                             ),
-                            const SizedBox(height: 8),
-                            Text('${index + 1}'),
-                          ],
-                        ),
+                          ),
+                        ],
+                      )
+                    : const PreferredSize(
+                        preferredSize: Size(0, 0),
+                        child: SizedBox(),
                       ),
-                    );
-                  },
-                  onPageChanged: (int page) {
-                    _mainSettingsBox.put(widget.surahKey, page);
-                  },
-                ),
+              ),
+              body: PageView.builder(
+                controller: _surahPageController,
+                reverse: true,
+                itemCount: AppStrings.surahsLists[widget.surahNumber].length,
+                itemBuilder: (BuildContext context, int index) {
+                  final String surahPage = AppStrings.surahsLists[widget.surahNumber][index];
+                  return Center(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'assets/quran/$surahPage.png',
+                            fit: BoxFit.cover,
+                            color: appColors.inverseSurface.withOpacity(
+                              surahSettings.getFontContrast / 100,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text('${index + 1}'),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                onPageChanged: (int page) {
+                  _mainSettingsBox.put(widget.surahKey, page);
+                },
               ),
             ),
           ),
