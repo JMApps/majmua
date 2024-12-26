447c6328b3edaba426e129d0a288e3cb57b65b8d
jmapps <jmappsmuslim@gmail.com>
2024-12-26 21:05:12 +0300

    Added app string consts and other

---
+++ lib/core/strings/app_string_constraints.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+class AppStringConstraints {
+  static const String fontGilroy = 'Gilroy';
+  static const String fontHafs = 'Hafs';
+
+  static const String keyAppSettingsBox = 'key_app_settings_box';
+
+  static const String keyAppThemeColor = 'key_app_theme_color';
+  static const String keyAppThemeModeIndex = 'key_app_theme_mode_index';
+  static const String keyAppLocaleIndex = 'key_app_locale_index';
+
+  static const String iconSettings = 'settings';
+  static const String iconNotifications = 'notifications';
+  static const String iconLocation = 'location';
+  static const String iconCalendar = 'calendar';
+  static const String iconQiblah = 'qiblah';
+  static const String iconAqsa = 'aqsa';
+  static const String iconHands = 'hands';
+
+  static const String timeSunrise = 'sunrise';
+  static const String timeMiddleNight = 'middle_night';
+  static const String timeLastThird = 'last_third_part';
+
+  static const List<Locale> appLocales = [
+    Locale('ru', 'RU'),
+    Locale('en', 'US'),
+  ];
+}
