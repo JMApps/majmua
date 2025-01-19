ae40868802115bccaad3b36d6053749b74ce290b
jmapps <jmappsmuslim@gmail.com>
2025-01-19 15:15:43 +0300

    Added library button with styles

---
+++ lib/presentation/library/widgets/library_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../../../core/strings/app_string_constraints.dart';
+import '../../../core/styles/app_styles.dart';
+
+class LibraryButton extends StatelessWidget {
+  const LibraryButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final appColors = Theme.of(context).colorScheme;
+    return Padding(
+      padding: AppStyles.mardingWithoutTopMini,
+      child: OutlinedButton.icon(
+        onPressed: () {
+          HapticFeedback.lightImpact();
+        },
+        iconAlignment: IconAlignment.end,
+        icon: const Icon(Icons.menu_book_rounded),
+        label: const Text(
+          'Библиотека',
+          style: TextStyle(
+            fontSize: 15.0,
+            fontFamily: AppStringConstraints.fontGilroyMedium,
+          ),
+        ),
+        style: ButtonStyle(
+          overlayColor: WidgetStateProperty.all(appColors.tertiaryContainer),
+          side: WidgetStateProperty.all<BorderSide>(
+            BorderSide(
+              width: 2.5,
+              color: appColors.primaryContainer,
+            ),
+          ),
+          shape: WidgetStateProperty.all(AppStyles.mainShapeMini),
+        ),
+      ),
+    );
+  }
+}
